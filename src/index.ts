2d03cedface05b02873020b3e5187dd451b04a90
Luke Edwards <luke.edwards05@gmail.com>
2020-12-23 14:26:50 -0800

    perf: boost 🚀 (#1)

--- src/index.ts
+++ src/index.ts
@@ -1,94 +1,95 @@
-type LogFunc = (message?: string | Error, ...args: unknown[]) => void;
-type LogHook = (event: LogEvent) => LogEvent | void;
-type LogVerbs = 'error' | 'warn' | 'debug' | 'info' | 'log';
-type DiaryInstance = ReturnType<typeof diary>;
-type VerbLevels = keyof typeof verb_levels;
-type VerbLevelValues = typeof verb_levels[VerbLevels];
+// TODO? missing "fatal" usage (60, '✗')
 
-export type LogEvent = {
+type LogHook = (event: LogEvent) => LogEvent | void;
+type LogFunc = (message?: LogMessage, ...args: unknown[]) => void;
+type LogVerbs = 'error' | 'warn' | 'debug' | 'info' | 'log';
+type DiaryInstance = Record<LogVerbs, LogFunc>;
+type LogMessage = Error | string;
+
+type LogValues = typeof LEVELS[keyof typeof LEVELS];
+
+export interface LogEvent {
 	name: string;
 	level: LogVerbs;
 	message: string | Error;
 	extra: unknown[];
-};
+}
 
 const is_node = typeof process < 'u' && typeof process.stdout < 'u';
-const global_ident = {};
-const hooks = new WeakMap([[global_ident, []]]);
-const verb_levels = { f: 60, e: 50, w: 40, i: 30, d: 20, l: 10 } as const;
-const levels_symbol: Record<VerbLevels, string> = { f: '✗', e: '✗', w: '‼', i: 'ℹ', d: '●', l: '◆' } as const;
 
-let active_level: VerbLevelValues = verb_levels.l;
+const global_ident = {} as DiaryInstance;
+const hooks = new WeakMap<DiaryInstance, LogHook[]>([
+	[global_ident, []]
+]);
 
+const LEVELS = { error: 50, warn: 40, info: 30, debug: 20, log: 10 } as const;
+
+let active_level: LogValues = LEVELS.log;
 export const setLevel = (level: LogVerbs) => {
-	active_level = verb_levels[level[0] as VerbLevels];
+	active_level = LEVELS[level];
 };
 
-const logger = (name: string, hook_ref: any) => (level: LogVerbs): LogFunc => (
-	message,
-	...extra
-) => {
+// read `localstorage`/`env` for scope "name"s allowed to log
+const toRegExp = (x: string) => new RegExp(x.replace(/\*/g, '.*') + '$');
+const allows: RegExp[] = ((is_node ? process.env.DEBUG : localStorage.getItem('DEBUG')) || '').split(/[\s,]+/).map(toRegExp);
+
+function logger(
+	ctx: DiaryInstance,
+	name: string,
+	level: LogVerbs,
+	symbol: string,
+	message: LogMessage,
+	...extra: unknown[]
+): void {
 	// Check if `setLevel` prohibits processing this
-	if (!(verb_levels[level[0] as VerbLevels] >= active_level)) return;
+	if (LEVELS[level] < active_level) return;
 
-	// Next we check if `localstorage`/`env` allows this "scope" to log
-	const parts = ((is_node ? process.env.DEBUG : localStorage.getItem('DEBUG')) || '')
-		.split(/[\s,]+/)
-		.some(i => new RegExp(`${i.replace(/\*/g, '.*')}$`).test(name));
-	if (!parts) return;
+	// Is this "scope" allowed to log?
+	if (!allows.some(x => x.test(name))) return;
 
-	let hook: LogHook,
-		r: LogEvent | void = { name, level, message, extra };
+	let r: LogEvent = { name, level, message, extra };
 
 	// Handle errors specially
-	if (r.level === 'error' && Object.prototype.toString.call(message) === '[object Error]') {
-		const e = (r.message as Error);
-		r.message = e.message;
-		r.extra.unshift(e);
+	if (r.level === 'error' && message instanceof Error) {
+		r.message = message.message;
+		r.extra.unshift(message);
 	}
 
 	// Loop through all middlewares
-	for (hook of [].concat(hooks.get(hook_ref), hooks.get(global_ident))) if (!(r = hook(r))) return;
+	for (let hook of [].concat(hooks.get(ctx), hooks.get(global_ident))) {
+		if (!(r = hook(r))) return;
+	}
 
 	// Output
+	let label = '';
+	if (is_node) label = `${symbol} ${level.padEnd(6, ' ')}`;
+	if (name) label += `[${name}] `;
 
-	let label = '',
-		write_fn = console.log;
-
-	if (!!~Object.keys(console).indexOf(r.level)) write_fn = console[r.level];
-
-	if (is_node) label = `${levels_symbol[r.level[0] as VerbLevels]} ${r.level.padEnd(6, ' ')}`;
-	if (r.name) label += `[${r.name}] `;
-
-	write_fn(`${label}${message}`, ...r.extra);
-};
+	(console[level] || console.log)(`${label}${message}`, ...r.extra);
+}
 
 export const middleware = (
 	handler: LogHook,
-	diary_instance: ReturnType<typeof diary> | object = global_ident,
+	diary_instance: DiaryInstance = global_ident,
 ) => {
 	hooks.get(diary_instance).push(handler);
 };
 
-export function diary(name: string): Record<LogVerbs, LogFunc> {
-	const fns: DiaryInstance = { } as DiaryInstance;
-	const logger_for = logger(name, fns);
-
-	fns.error = logger_for('error');
-	fns.warn = logger_for('warn');
-	fns.debug = logger_for('debug');
-	fns.info = logger_for('info');
-	fns.log = logger_for('log');
-
-	hooks.set(fns, []);
-
-	return fns;
+export function diary(name: string): DiaryInstance {
+	const ctx = {} as DiaryInstance;
+	ctx.warn = logger.bind(0, ctx, name, 'warn', '‼');
+	ctx.error = logger.bind(0, ctx, name, 'error', '✗');
+	ctx.debug = logger.bind(0, ctx, name, 'debug', '●');
+	ctx.info = logger.bind(0, ctx, name, 'info', 'ℹ');
+	ctx.log = logger.bind(0, ctx, name, 'log', '◆');
+	hooks.set(ctx, []);
+	return ctx;
 }
 
 const default_diary = diary('');
 
-export const error = default_diary.error;
 export const warn = default_diary.warn;
+export const error = default_diary.error;
 export const debug = default_diary.debug;
 export const info = default_diary.info;
 export const log = default_diary.log;
