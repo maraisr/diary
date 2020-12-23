d8b4ed4e367f803db9a32832c9dd0414de732395
Luke Edwards <luke.edwards05@gmail.com>
2020-12-22 23:51:37 -0800

    refactor: rework `LEVELS` const; add `symbol` param; return instance directly

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
-type LogFunc = (message?: string | Error, ...args: unknown[]) => void;
+// TODO? missing "fatal" usage (60, '✗')
+
 type LogHook = (event: LogEvent) => LogEvent | void;
+type LogFunc = (message?: string | Error, ...args: unknown[]) => void;
 type LogVerbs = 'error' | 'warn' | 'debug' | 'info' | 'log';
 type DiaryInstance = Record<LogVerbs, LogFunc>;
-type VerbLevels = keyof typeof verb_levels;
-type VerbLevelValues = typeof verb_levels[VerbLevels];
+
+type LogValues = typeof LEVELS[keyof typeof LEVELS];
 
 export interface LogEvent {
 	name: string;
@@ -13,60 +15,64 @@ export interface LogEvent {
 }
 
 const is_node = typeof process < 'u' && typeof process.stdout < 'u';
-const verb_levels = { f: 60, e: 50, w: 40, i: 30, d: 20, l: 10 } as const;
-const levels_symbol: Record<VerbLevels, string> = { f: '✗', e: '✗', w: '‼', i: 'ℹ', d: '●', l: '◆' } as const;
 
 const global_ident = {} as DiaryInstance;
 const hooks = new WeakMap<DiaryInstance, LogHook[]>([
 	[global_ident, []]
 ]);
 
-let active_level: VerbLevelValues = verb_levels.l;
+const LEVELS = { error: 50, warn: 40, info: 30, debug: 20, log: 10 } as const;
 
+let active_level: LogValues = LEVELS.log;
 export const setLevel = (level: LogVerbs) => {
-	active_level = verb_levels[level[0] as VerbLevels];
+	active_level = LEVELS[level];
 };
 
 const toRegExp = (x: string) => new RegExp(x.replace(/\*/g, '.*') + '$');
 
-const logger = (name: string, hook_ref: any) => {
+function logger(name: string): DiaryInstance {
 	// TODO: hoist this to global side effect? do once
 	// read `localstorage`/`env` for scope "name"s allowed to log
 	const allows: RegExp[] = ((is_node ? process.env.DEBUG : localStorage.getItem('DEBUG')) || '').split(/[\s,]+/).map(toRegExp);
 
-	return (level: LogVerbs): LogFunc => (
-		message,
-		...extra
-	) => {
-		// Check if `setLevel` prohibits processing this
-		if (!(verb_levels[level[0] as VerbLevels] >= active_level)) return;
+	function writer(level: LogVerbs, symbol: string): LogFunc {
+		return (message, ...extra) => {
+			// Check if `setLevel` prohibits processing this
+			if (LEVELS[level] < active_level) return;
 
-		// Is this "scope" allowed to log?
-		if (!allows.some(x => x.test(name))) return;
+			// Is this "scope" allowed to log?
+			if (!allows.some(x => x.test(name))) return;
 
-		let r: LogEvent = { name, level, message, extra };
+			let r: LogEvent = { name, level, message, extra };
 
-		// Handle errors specially
-		if (r.level === 'error' && message instanceof Error) {
-			r.message = message.message;
-			r.extra.unshift(message);
+			// Handle errors specially
+			if (r.level === 'error' && message instanceof Error) {
+				r.message = message.message;
+				r.extra.unshift(message);
+			}
+
+			// Loop through all middlewares
+			for (let hook of [].concat(hooks.get(hook_ref), hooks.get(global_ident))) {
+				if (!(r = hook(r))) return;
+			}
+
+			// Output
+
+			let label = '';
+			if (is_node) label = `${symbol} ${level.padEnd(6, ' ')}`;
+			if (r.name) label += `[${r.name}] `;
+
+			(console[level] || console.log)(`${label}${message}`, ...r.extra);
 		}
-
-		// Loop through all middlewares
-		for (let hook of [].concat(hooks.get(hook_ref), hooks.get(global_ident))) {
-			if (!(r = hook(r))) return;
-		}
-
-		// Output
-
-		let label = '';
-		const write_fn = console[r.level] || console.log;
-
-		if (is_node) label = `${levels_symbol[r.level[0] as VerbLevels]} ${r.level.padEnd(6, ' ')}`;
-		if (r.name) label += `[${r.name}] `;
-
-		write_fn(`${label}${message}`, ...r.extra);
 	}
+
+	return {
+		warn: writer('warn', '‼'),
+		error: writer('error', '✗'),
+		debug: writer('debug', '●'),
+		info: writer('info', 'ℹ'),
+		log: writer('log', '◆'),
+	};
 };
 
 export const middleware = (
@@ -77,18 +83,9 @@ export const middleware = (
 };
 
 export function diary(name: string): DiaryInstance {
-	const fns: DiaryInstance = { } as DiaryInstance;
-	const logger_for = logger(name, fns);
-
-	fns.error = logger_for('error');
-	fns.warn = logger_for('warn');
-	fns.debug = logger_for('debug');
-	fns.info = logger_for('info');
-	fns.log = logger_for('log');
-
-	hooks.set(fns, []);
-
-	return fns;
+	const ctx = logger(name);
+	hooks.set(ctx, []);
+	return ctx;
 }
 
 const default_diary = diary('');
