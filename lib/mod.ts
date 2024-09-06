59a33f7376c746fc0161ae3caca17a6bc4b68ad2
Marais Rossouw <me@marais.co>
2024-09-06 12:50:05 +1000

    :construction: WIP

--- lib/mod.ts
+++ lib/mod.ts
@@ -1,7 +1,47 @@
 import type { Dict, Pretty, Props } from './utils.ts';
 
+/**
+ * TODO
+ *
+ * @module
+ */
+
+/**
+ * The level to tag a log event with.
+ *
+ * Logging levels are a cornerstone of any logging system, they help declare importance of a log message.
+ * Diary therefore treats this piece of metadata as a first-class citizen, however does not do anything special with them — and is forwarded directly to your {@link OnEmitFn}.
+ *
+ * A quick rundown of the levels:
+ *
+ * - `log` — is the default level, it is used for general logging.
+ * - `debug` — is typically used during the debugging phase, and may not be present in production.
+ * - `info` — is often used to represent a significant event in the system, like a user login.
+ * - `warn` — is used to represent a potential problem in the system, like memory cache eviction shorter than its TTL.
+ * - `error` — is used to represent a problem in the system, like a failure to cache something.
+ * - `fatal` — is used to represent a catastrophic failure in the system, like a failure to connect to the database.
+ */
 export type Level = 'log' | 'debug' | 'info' | 'warn' | 'error' | 'fatal';
 
+/**
+ * A representing container for a log event entry.
+ *
+ * Every {@link diary} logged event will be represented as a tuple, taking the shape of:
+ *
+ * ```ts
+ * [level: Level, message: string, attributes?: Dict]
+ * ```
+ *
+ * it has the intended design to be destructured for further consumption.
+ *
+ * @example Usage
+ *
+ * ```ts
+ * const [level, message, attributes] = log_event;
+ * if (level === 'fatal') exit(1);
+ * stdout.write(`${message} ${JSON.stringify(attributes)}`);
+ * ```
+ */
 export type LogEvent = Parameters<OnEmitFn>;
 
 export interface OnEmitFn {
